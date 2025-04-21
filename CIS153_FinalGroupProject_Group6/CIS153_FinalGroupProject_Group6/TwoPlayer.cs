06304e46dd2c91e88ff307565103596a9b87efc1
Dragongpv <grangerpv@gmail.com>
2025-04-21 18:51:01 -0400

    Player 2 checks/gameover functionality
    added checks for player 2 winning
    game will now end when a player wins and board can no longer be interacted with

--- CIS153_FinalGroupProject_Group6/CIS153_FinalGroupProject_Group6/TwoPlayer.cs
+++ CIS153_FinalGroupProject_Group6/CIS153_FinalGroupProject_Group6/TwoPlayer.cs
@@ -16,7 +16,7 @@ namespace CIS153_FinalGroupProject_Group6
         int playerTurn = 1; //1 is player one being red and 2 is player 2/AI being yellow
         int turnsTaken = 0; //keeps track of how many turns have been taken
         Board board = new Board();
-
+        bool win = false;
 
         public form_twoPlayer()
         {
@@ -35,38 +35,31 @@ namespace CIS153_FinalGroupProject_Group6
         //func called when button is clicked
         private void Button_Click(object sender, EventArgs e)
         {
-            //store the sender (button that was clicked) as clicked button
-            Button clickedButton = sender as Button;
-            if (clickedButton != null)
+            if(!win)
             {
-                //takes btn name and stores in string
-                string buttonName = clickedButton.Name;
-                //Console.WriteLine(buttonName);
-
-                //grabs x cord out of name (4th index in string)
-                int x = int.Parse(buttonName[4].ToString());
-                //grabs y cord out of name (5th index in string)
-                int y = int.Parse(buttonName[5].ToString());
-                //Console.WriteLine(x);
-                //Console.WriteLine(y);
-
-
-                //check that cell is empty at position
-                if (board.getCell(x, y).getState() == 0)
+                //store the sender (button that was clicked) as clicked button
+                Button clickedButton = sender as Button;
+                if (clickedButton != null)
                 {
-                    //check if piece falls and set state
-                    checkFall(x, y, clickedButton);
+                    //takes btn name and stores in string
+                    string buttonName = clickedButton.Name;
+                    //Console.WriteLine(buttonName);
 
+                    //grabs x cord out of name (4th index in string)
+                    int x = int.Parse(buttonName[4].ToString());
+                    //grabs y cord out of name (5th index in string)
+                    int y = int.Parse(buttonName[5].ToString());
+                    //Console.WriteLine(x);
+                    //Console.WriteLine(y);
+
+                    //check that cell is empty at position
+                    if (board.getCell(x, y).getState() == 0)
+                    {
+                        //check if piece falls and set state
+                        checkFall(x, y, clickedButton);
+                    }
                 }
-
-
-
-
-
             }
-
-
-
         }
 
         private void checkFall(int xCord, int yCord, Button clickedButton)
@@ -138,11 +131,11 @@ namespace CIS153_FinalGroupProject_Group6
         private void checkWin(int xCord, int yCord)
         {
             //check if 4 in a row in all directions
-            //if turn is 1, check for red
             //inRow will keep track of how many in a row
             int inRow = 0;
-            bool win = false;
-            if(playerTurn == 1)
+
+            //if turn is 1, check for red
+            if (playerTurn == 1)
             {
                 //left
                 inRow = 0;
@@ -260,7 +253,7 @@ namespace CIS153_FinalGroupProject_Group6
 
                 //diagonal right up
                 inRow = 0;
-                for (int yi = yCord, xi = xCord; yi >= 0 && xi >= 0; yi--, xi++)
+                for (int yi = yCord, xi = xCord; yi >= 0 && xi <= 6; yi--, xi++)
                 {
                     if (win)
                     {
@@ -348,10 +341,214 @@ namespace CIS153_FinalGroupProject_Group6
                 }
             }
 
+            //if it is player 2's turn
             if(playerTurn == 2)
             {
+                //left
+                inRow = 0;
+                for (int xi = xCord; xi >= 0; xi--)
+                {
+                    if (win)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("xi : " + xi);
+                    if (board.getCell(xi, yCord).getState() == 2)
+                    {
+                        //if state is same color add to inRow
+                        inRow++;
+                        Console.WriteLine("inRow : " + inRow);
+                    }
+                    else if (board.getCell(xi, yCord).getState() == 1 || board.getCell(xi, yCord).getState() == 0)
+                    {
+                        //if state is not same color or empty
+                        break;
+                    }
+                    if (inRow == 4)
+                    {
+                        //set win to true
+                        win = true;
+                        Console.WriteLine("4 in a row Left");
+                    }
+                }
+
+                //right
+                inRow = 0;
+                for (int xi = xCord; xi <= 6; xi++)
+                {
+                    if (win)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("xi : " + xi);
+                    if (board.getCell(xi, yCord).getState() == 2)
+                    {
+                        //if state is same color add to inRow
+                        inRow++;
+                        Console.WriteLine("inRow : " + inRow);
+                    }
+                    else if (board.getCell(xi, yCord).getState() == 1 || board.getCell(xi, yCord).getState() == 0)
+                    {
+                        //if state is not same color or empty
+                        break;
+                    }
+                    if (inRow == 4)
+                    {
+                        //set win to true
+                        win = true;
+                        Console.WriteLine("4 in a row Right");
+                    }
+                }
+                //up
+                //dont need a straight up
+
+                //down
+                inRow = 0;
+                for (int yi = yCord; yi <= 5; yi++)
+                {
+                    if (win)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("yi : " + yi);
+                    if (board.getCell(xCord, yi).getState() == 2)
+                    {
+                        //if state is same color add to inRow
+                        inRow++;
+                        Console.WriteLine("inRow : " + inRow);
+                    }
+                    else if (board.getCell(xCord, yi).getState() == 1 || board.getCell(xCord, yi).getState() == 0)
+                    {
+                        //if state is not same color or empty
+                        break;
+                    }
+                    if (inRow == 4)
+                    {
+                        //set win to true
+                        win = true;
+                        Console.WriteLine("4 in a row down");
+                    }
+                }
+
+                //diangonal left up
+                inRow = 0;
+                for (int yi = yCord, xi = xCord; yi >= 0 && xi >= 0; yi--, xi--)
+                {
+                    if (win)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("xi : " + xi + " yi : " + yi);
+                    if (board.getCell(xi, yi).getState() == 2)
+                    {
+                        //if state is same color add to inRow
+                        inRow++;
+                        Console.WriteLine("inRow : " + inRow);
+                    }
+                    else if (board.getCell(xi, yi).getState() == 1 || board.getCell(xi, yi).getState() == 0)
+                    {
+                        //if state is not same color or empty
+                        break;
+                    }
+                    if (inRow == 4)
+                    {
+                        //set win to true
+                        win = true;
+                        Console.WriteLine("4 in a row up and left");
+                    }
+                }
+
+                //diagonal right up
+                inRow = 0;
+                for (int yi = yCord, xi = xCord; yi >= 0 && xi <= 6; yi--, xi++)
+                {
+                    if (win)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("xi : " + xi + " yi : " + yi);
+                    if (board.getCell(xi, yi).getState() == 2)
+                    {
+                        //if state is same color add to inRow
+                        inRow++;
+                        Console.WriteLine("inRow : " + inRow);
+                    }
+                    else if (board.getCell(xi, yi).getState() == 1 || board.getCell(xi, yi).getState() == 0)
+                    {
+                        //if state is not same color or empty
+                        break;
+                    }
+                    if (inRow == 4)
+                    {
+                        //set win to true
+                        win = true;
+                        Console.WriteLine("4 in a row up and right");
+                    }
+                }
+
+                //diagonal left down
+                inRow = 0;
+                for (int yi = yCord, xi = xCord; yi <= 5 && xi >= 0; yi++, xi--)
+                {
+                    if (win)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("xi : " + xi + " yi : " + yi);
+                    if (board.getCell(xi, yi).getState() == 2)
+                    {
+                        //if state is same color add to inRow
+                        inRow++;
+                        Console.WriteLine("inRow : " + inRow);
+                    }
+                    else if (board.getCell(xi, yi).getState() == 1 || board.getCell(xi, yi).getState() == 0)
+                    {
+                        //if state is not same color or empty
+                        break;
+                    }
+                    if (inRow == 4)
+                    {
+                        //set win to true
+                        win = true;
+                        Console.WriteLine("4 in a row down and left");
+                    }
+                }
+                //diagonal right down
+                inRow = 0;
+                for (int yi = yCord, xi = xCord; yi <= 5 && xi <= 6; yi++, xi++)
+                {
+                    if (win)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("xi : " + xi + " yi : " + yi);
+                    if (board.getCell(xi, yi).getState() == 2)
+                    {
+                        //if state is same color add to inRow
+                        inRow++;
+                        Console.WriteLine("inRow : " + inRow);
+                    }
+                    else if (board.getCell(xi, yi).getState() == 1 || board.getCell(xi, yi).getState() == 0)
+                    {
+                        //if state is not same color or empty
+                        break;
+                    }
+                    if (inRow == 4)
+                    {
+                        //set win to true
+                        win = true;
+                        Console.WriteLine("4 in a row up and right");
+                    }
+                }
+
+                if (win)
+                {
+                    //win condition goes here
+                    Console.WriteLine("Player 2 wins!");
+                }
 
             }
+            
         }
     }
 }
